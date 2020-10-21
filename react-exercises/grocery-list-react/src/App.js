f99983c91f6a264dbdcdf4f1cec781b600d0f340
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-10-21 18:51:20 +0530

    feat: Render items in the list

--- react-exercises/grocery-list-react/src/App.js
+++ react-exercises/grocery-list-react/src/App.js
@@ -2,34 +2,88 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Item() {
+function Item({ item, quantity }) {
   return (
     <div className='card'>
-      <section className='item-name'>Potato</section>
-      <section className='item-quantity'>1</section>
+      <section className='item-name'>{item}</section>
+      <section className='item-quantity'>{quantity}</section>
     </div>
   );
+}
+
+function isItemAlreadyAdded(item, items) {
+  items.some(({ itemName, quantity }) => item === itemName);
+}
+
+function getAddedItem(item, items) {
+  items.forEach(({ item_name, quantity }, index) => {
+    if (item === item_name) {
+      return {
+        item,
+        quantity,
+        index,
+      };
+    }
+  });
 }
 
 function App() {
   const [item, setItem] = useState('');
   const [items, setItems] = useState([]);
 
+  const addItemToList = (item, items) => {
+    if (isItemAlreadyAdded(item, items)) {
+      const { item: itemName, quantity, index } = getAddedItem(item, items);
+      const item_obj = {
+        item: itemName,
+        quantity: quantity + 1,
+      };
+
+      const updatedItems = items
+        .splice(0, index)
+        .concat(items.splice(index + 1, -1));
+
+      setItems([...updatedItems, item_obj]);
+      setItem('');
+    } else {
+      const item_obj = {
+        item,
+        quantity: 1,
+      };
+
+      setItems([...items, item_obj]);
+      setItem('');
+    }
+  };
+
+  console.log(items);
   return (
     <div className='App'>
       <h1 className='heading'>Grocery List</h1>
 
       <section className='input-area'>
-        <input type='text' name='item-name' id='item-name' />
-        <button>Add Item</button>
+        <input
+          type='text'
+          name='item-name'
+          id='item-name'
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={() => addItemToList(item, items)}>Add Item</button>
       </section>
 
       <section className='clear-button-section'>
-        <button className='clear-button'>Clear List</button>
+        {items.length > 0 ? (
+          <button className='clear-button' onClick={() => setItems([])}>
+            Clear List
+          </button>
+        ) : null}
       </section>
 
       <section className='item-list'>
-        <Item />
+        {items.map(({ item, quantity }) => (
+          <Item item={item} quantity={quantity} key={item} />
+        ))}
       </section>
     </div>
   );
