1bbbd00997203375083d7c3947fbe102f7d9c627
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-10-21 18:22:48 +0530

    feat: Prepare basic layout

---
+++ react-exercises/grocery-list-react/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Item() {
+  return (
+    <div className='card'>
+      <section className='item-name'>Potato</section>
+      <section className='item-quantity'>1</section>
+    </div>
+  );
+}
+
+function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  return (
+    <div className='App'>
+      <h1 className='heading'>Grocery List</h1>
+
+      <section className='input-area'>
+        <input type='text' name='item-name' id='item-name' />
+        <button>Add Item</button>
+      </section>
+
+      <section className='item-list'>
+        <Item />
+      </section>
+    </div>
+  );
+}
+
+export default App;
