ef1ffa05af0a5ff62a74db0c93fec337c342e931
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-10-28 02:59:44 +0530

    Solve use-key-press

--- react-exercises/use-key-press/src/App.js
+++ react-exercises/use-key-press/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getKey(key) {
+  if (!isControlKey(key)) {
+    return key;
+  } else {
+    return key.replace('<', '').replace('>', '');
+  }
+}
+
+function isControlKey(key) {
+  if (key.includes('<')) {
+    return true;
+  }
+
+  return false;
+}
+
+function useKeyPress(key) {
+  const [isKeyPressed, setKeyPressed] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('keyup', ({ key: keyPressed }) => {
+      const cleanedKey = getKey(key);
+      if (cleanedKey === keyPressed.toLowerCase()) {
+        setKeyPressed(true);
+      } else {
+        setKeyPressed(false);
+      }
+    });
+  });
+
+  return isKeyPressed;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const hKeyPressed = useKeyPress('<enter>');
+  return hKeyPressed ? <h1>Pressed</h1> : <h2>Not pressed</h2>;
 }
 
 export default App;
